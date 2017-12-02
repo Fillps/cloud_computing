949ee8ba2be0293ef2c4e5918f8c00cc6cf659e6
Fillps <filipess@live.com>
2017-12-02 00:59:31 -0200

    trocando a configuração padrão para production

--- runserver.py
+++ runserver.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from cloud_computing import create_app
 
-app = create_app('../configs/development.py')
+app = create_app('../configs/production.py')
 
 if __name__ == '__main__':
     app.run()
