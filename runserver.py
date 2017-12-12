9537997d96b4be8849ff5d19cf1ef91631bef347
jefersonfg <jefersonfguimaraes@gmail.com>
2017-12-12 11:12:25 -0200

    Alteração na forma de inicialização do aplicativo.
    * Bug: cadastro de usuário tenta cadastrar usuário com id = 1, que já existe

--- runserver.py
+++ runserver.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from cloud_computing import create_app
+from cloud_computing.app_factory import AppFactory
 
-app = create_app('../configs/production.py')
+# app = create_app('../configs/production.py')
+
+factory = AppFactory()
+app = factory.get_app()
 
 if __name__ == '__main__':
     app.run()
