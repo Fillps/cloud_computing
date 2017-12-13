28f59c5f68fee7cc7bef0d03350b7fb1a42ff576
jfguimaraes <jefersonfguimaraes@gmail.com>
2017-12-13 10:22:03 -0200

    Adicionados primeiros três planos no carousel da página inicial.
    * Busca por planos retorna só planos não-customizados
    * Ainda é preciso determinar uma ordem para exibição dos planos na interface

--- cloud_computing/controller/controller.py
+++ cloud_computing/controller/controller.py
@@ -8,7 +8,8 @@ class Controller(object):
     @staticmethod
     def get_plans():
         """Queries the objects of type Plan on the database."""
-        return Plan.query.all()
+        # TODO Determine the order of the list to show on front-end
+        return Plan.query.filter_by(is_public=True)
 
     @staticmethod
     def get_plan_by_item_name(item_name):
