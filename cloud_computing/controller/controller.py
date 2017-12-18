93daf658d25100dcfc4c8572b40da6f422bd878a
jfguimaraes <jefersonfguimaraes@gmail.com>
2017-12-17 22:03:25 -0200

    Página de detalhes do plano agora exibe todos os detalhes do plano

--- cloud_computing/controller/controller.py
+++ cloud_computing/controller/controller.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from cloud_computing.model.models import Plan
+from cloud_computing.model.models import Plan, Gpu, Ram, Hd, PlanGpu, PlanRam, PlanHd
 
 
 class Controller:
@@ -15,3 +15,21 @@ class Controller:
     def get_plan_by_slug_url(slug_url):
         """Queries the database for the Plan of slug 'slug_url'."""
         return Plan.query.filter_by(slug_url=slug_url).first()
+
+    @staticmethod
+    def get_plan_gpu_list(plan_id):
+        """Queries the database for the gpus related to the plan_id."""
+        return Gpu.query.join(PlanGpu, Gpu.model == PlanGpu.gpu_model)\
+            .filter(plan_id == PlanGpu.plan_id)
+
+    @staticmethod
+    def get_plan_ram_list(plan_id):
+        """Queries the database for the ram memories related to the plan_id."""
+        return Ram.query.join(PlanRam, Ram.model == PlanRam.ram_model)\
+            .filter(plan_id == PlanRam.plan_id)
+
+    @staticmethod
+    def get_plan_hd_list(plan_id):
+        """Queries the database for the hds related to the plan_id."""
+        return Hd.query.join(PlanHd, Hd.model == PlanHd.hd_model)\
+            .filter(plan_id == PlanHd.plan_id)
